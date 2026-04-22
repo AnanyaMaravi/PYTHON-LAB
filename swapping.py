a47caf65923772fa37f03c156a2a0c910ca3a63c
AnanyaMaravi <ananyamaravi1104@gmail.com>
2026-04-23 00:32:47 +0530

    Added swapping program

---
+++ swapping.py
@@ -0,0 +1,14 @@
+# Program to demonstrate variable assignment and swapping
+
+# Assign values
+a = 5
+b = 10
+
+print("Before swapping:")
+print("a =", a, "b =", b)
+
+# Swapping values
+a, b = b, a
+
+print("After swapping:")
+print("a =", a, "b =", b)
