decf278d06ca00fee1ef2f4a1afe237535471898
AnanyaMaravi <ananyamaravi1104@gmail.com>
2026-04-23 00:49:42 +0530

    string_operation progran

---
+++ string_operation.py
@@ -0,0 +1,13 @@
+# Program for string operations
+
+text = input("Enter a string: ")
+
+# Slicing
+print("First 3 characters:", text[:3])
+
+# Concatenation
+new_text = text + " Python"
+print("After concatenation:", new_text)
+
+# Reversing string
+print("Reversed string:", text[::-1])
