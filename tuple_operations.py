751c5ce629ee2e2cb8a9afc376764bf9444a46a4
AnanyaMaravi <ananyamaravi1104@gmail.com>
2026-04-23 10:15:02 +0530

    Added tuple_operations

---
+++ tuple_operations.py
@@ -0,0 +1,11 @@
+# Program to demonstrate tuple and immutability
+
+t = (1, 2, 3, 4)
+
+print("Tuple:", t)
+
+# Access element
+print("First element:", t[0])
+
+# Trying to modify tuple (will give error)
+# t[0] = 10   # Uncommenting this will show immutability error
