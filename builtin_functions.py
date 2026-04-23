c67b22500788e1ec50a2c52d488f2208534e334f
AnanyaMaravi <ananyamaravi1104@gmail.com>
2026-04-23 10:17:42 +0530

    built in functions

---
+++ builtin_functions.py
@@ -0,0 +1,19 @@
+# Program to demonstrate built-in functions
+
+lst = [5, 2, 9, 1]
+
+# List functions
+print("Length:", len(lst))
+print("Max:", max(lst))
+print("Min:", min(lst))
+print("Sorted:", sorted(lst))
+
+# String functions
+text = "python"
+print("Upper:", text.upper())
+print("Lower:", text.lower())
+
+# Dictionary functions
+d = {"a": 1, "b": 2}
+print("Keys:", d.keys())
+print("Values:", d.values())
