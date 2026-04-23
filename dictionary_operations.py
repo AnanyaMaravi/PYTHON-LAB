2d4e7730b3488e66d05398d901a2563a5a4735ed
AnanyaMaravi <ananyamaravi1104@gmail.com>
2026-04-23 10:16:26 +0530

    dictionary_operations program

---
+++ dictionary_operations.py
@@ -0,0 +1,15 @@
+# Program for dictionary operations
+
+d = {"name": "Ananya", "age": 20}
+
+# Add
+d["course"] = "BCA"
+print("After adding:", d)
+
+# Update
+d["age"] = 20
+print("After updating:", d)
+
+# Delete
+del d["name"]
+print("After deleting:", d)
